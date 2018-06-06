73eae3a567051a4279c563d5d03d827135890900
Russ Lascala <russlascala@gmail.com>
2018-06-06 19:44:45 -0400

    added to the responsiveness of the site. I added a media querq to fix the navigation on the footer. I also fixed the title at the top of the page. I will consider changing font size to pt for the bootom nab

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,11 @@ $(document).ready(function () {
         var iHeight = $(".topImg").height();
         //var imgHeight = height(initiate);
         if ($(document).scrollTop() > iHeight) { // check if user scrolled more than 50 from top of the browser window
-            $(".navbar-fixed-top").css("background-color", "#ddbbac"); // if yes, then change the color of class "navbar-fixed-top"to #ddbbac or maybe (#c48a80) 
-            $(".navbar-fixed-top").css("padding-bottom", "18px");
+            $(".fixed-top").css("background-color", "#ddbbac"); // if yes, then change the color of class "fixed-top"to #ddbbac or maybe (#c48a80) 
+            $(".fixed-top").css("padding-bottom", "18px");
+            
         } else {
-            $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
+            $(".fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
         }
     });
 })
